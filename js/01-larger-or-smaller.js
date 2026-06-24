2f28e96cb48a2d0cfeaf3ec228d1a74b647d293d
bdunnells <hieinnob@hotmail.com>
2026-06-24 06:24:44 +0000

    finish 01-larger-or-smaller.js

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -4,16 +4,20 @@
 //Then, display only the larger of the two within the browser window. 
 //Don’t forget to handle the fact that the two could be equal.
 
+//NOTE: must npm install prompt-sync before using prompt here
+// Import and initialize prompt-sync
+const prompt = require('prompt-sync')();
+
 let int1 = parseInt(prompt("Enter the first integer: "))
 let int2 = parseInt(prompt("Enter the second integer: "))
 
 if (int1 > int2) {
-    console.log("The first integer ${int1} is larger.")
+    console.log("The first integer", +int1, "is larger.")
 }
 if (int1 < int2) {
-    console.log("The first integer ${int2} is larger.")
+    console.log("The second integer", +int2, "is larger.")
 }
 if (int1 === int2) {
-    console.log("The first integer ${int1} is equal to the second integer ${int2}.")
+    console.log("The first integer", +int1, " is equal to the second integer ", +int2, ".\n")
 }
 
